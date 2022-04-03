26bbb24e86afcacb556a253c214acd27fa7441ec
abhijoshi23 <avjoshi1@myseneca.ca>
2022-04-02 23:21:41 -0400

    made changes in about me page

--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,6 +1,11 @@
 import "./aboutme.scss"
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Carousel from 'react-bootstrap/Carousel'
+import SkillBar from 'react-skillbars';
+const skills = [
+  {type: "Java", level: 85},
+  {type: "Javascript", level: 75},
+];
 
 export default function AboutMe() {
   return (
@@ -12,19 +17,20 @@ export default function AboutMe() {
       </p>
       <p className="para2"> Currently focused on learning and trying different things in development everyday
       </p>  
-      <div className="carasouel" style={{width:"100%",height:"60vh"}}>
+      <div className="carasouel">
 
-      <Carousel style={{top:"2%",left:"6%",width:"90%",height:"60vh"}}>
+      <Carousel className="innercarasouel" style={{top:"2%",left:"20%",width:"60%",height:"60vh",backgroundColor:"lightgrey"}}>
   <Carousel.Item>
    <div className="carousel-1"><h3>Education</h3></div>
    <div className="content">
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> 
+      <h3>Computer Programming and Analysis</h3>
+      <p>Studying computer programming and Analysis at Seneca college.</p> 
       </div>
   </Carousel.Item>
   <Carousel.Item>
   <div className="carousel-1"><h3>Skills</h3> </div>
   <div className="content">
+    <SkillBar skills={skills}/>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> 
       </div>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,78 @@
-import { useState } from "react";
 import "./contact.scss"
+import React from 'react';
+import axios from 'axios';
 
-export default function Contact() {
-  const [message,setMessage] = useState(false)
-  
-  const handleSubmit =(e)=>{ 
-    e.preventDefault();
-    setMessage(true)
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
   }
-  return (
-    <div className="contact" id="contact">
-      <div className="left">
+
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url:"http://localhost:3002/send",
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success') {
+        alert("Message Sent.");
+        this.resetForm()
+      } else if (response.data.status === 'fail') {
+        alert("Message failed to send.")
+      }
+    })
+  }
+
+  resetForm(){
+    this.setState({name: '', email: '', message: ''})
+  }
+
+  render() {
+    return(
+      <div className="contact" id="contact">
+        <div className="left">
         <img src="assets/downarrow.png" alt=""/>
       </div>
       <div className="right">
         <h2> Contact Me </h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email"/>
-          <textarea placeholder="message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thank You! I'll contact you ASAP!</span>}
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+          </div>
+          <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+          </div>
+          <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
+
+  onNameChange(event) {
+	  this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	  this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+	  this.setState({message: event.target.value})
+  }
 }
+
+export default App;
+
